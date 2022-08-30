c1691644a82d6da6595d8a52bca0a8f313d6e5ac
taishikato <taishi.k0903@gmail.com>
2022-08-29 22:54:26 -0700

    create CSCC files

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,5 @@
 import type { Dispatch, SetStateAction } from "react";
 import { memo } from "react";
-import { css, cx } from "@emotion/css";
 import { IoCreateOutline, IoTrashOutline } from "react-icons/io5";
 import { v4 as uuidv4 } from "uuid";
 import { getNoteList } from "../_utils/getNoteList";
@@ -68,17 +67,11 @@ const Sidebar = ({
               <li
                 key={note.key}
                 onClick={(e) => handler(e, note.key)}
-                className={cx([
-                  "flex items-center justify-between gap-x-2 w-full h-[50px] p-3 mb-3 box-border cursor-pointer",
+                className={`flex items-center justify-between gap-x-2 w-full h-[50px] p-3 mb-3 box-border cursor-pointer note-list-item ${
                   noteKey === note.key
                     ? "bg-slate-200 hover:bg-slate-200"
-                    : "hover:bg-slate-100",
-                  css`
-                    &:hover svg {
-                      display: block;
-                    },
-                  `,
-                ])}
+                    : "hover:bg-slate-100"
+                }`}
               >
                 {note.content.slice(0, 10) || "No Title"}
                 <IoTrashOutline
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./tailwind.css";
+import "./style.scss";
 import "./md-preview.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
