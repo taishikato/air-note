399ce2ab6c5bda92bb81e5c20e5a09b4ac5804f0
taishikato <taishi.k0903@gmail.com>
2022-09-02 00:52:20 -0700

    add sort

--- extension/src/_utils/getNoteList.ts
+++ extension/src/_utils/getNoteList.ts
@@ -5,22 +5,26 @@ const isProd = import.meta.env.PROD;
 export const getNoteList = isProd
   ? async (): Promise<NoteProperty[]> => {
       const notes: Note = await chrome.storage.sync.get(null);
-      return Object.keys(notes).map((key) => ({
-        key,
-        content: notes[key].content,
-        created_at: notes[key].created_at,
-        modified_at: notes[key].modified_at,
-      }));
+      return Object.keys(notes)
+        .map((key) => ({
+          key,
+          content: notes[key].content,
+          created_at: notes[key].created_at,
+          modified_at: notes[key].modified_at,
+        }))
+        .sort((a, b) => b.modified_at - a.modified_at);
     }
   : (): NoteProperty[] => {
       if (Object.keys(localStorage).length === 0) return [];
 
-      return Object.keys(localStorage).map((noteKey) => {
-        return {
-          key: noteKey,
-          content: JSON.parse(localStorage[noteKey] ?? "").content,
-          created_at: JSON.parse(localStorage[noteKey] ?? "").created_at,
-          modified_at: JSON.parse(localStorage[noteKey] ?? "").modified_at,
-        };
-      });
+      return Object.keys(localStorage)
+        .map((noteKey) => {
+          return {
+            key: noteKey,
+            content: JSON.parse(localStorage[noteKey] ?? "").content,
+            created_at: JSON.parse(localStorage[noteKey] ?? "").created_at,
+            modified_at: JSON.parse(localStorage[noteKey] ?? "").modified_at,
+          };
+        })
+        .sort((a, b) => b.modified_at - a.modified_at);
     };
