75376066f2550f85da5e3e519af75812a2d1998f
taishikato <taishi.k0903@gmail.com>
2022-09-02 00:45:44 -0700

    make save button bigger

--- extension/src/components/SaveButton.tsx
+++ extension/src/components/SaveButton.tsx
@@ -5,7 +5,7 @@ const SaveButton = ({
 }) => {
   return (
     <button
-      className="text-sm px-5 py-2 font-medium z-50 text-white rounded-full bg-slate-700 hover:bg-slate-900 shadow-button bg-transition"
+      className="text-sm px-6 py-3 font-medium z-50 text-white rounded-full bg-slate-700 hover:bg-slate-900 shadow-button bg-transition"
       onClick={handler}
     >
       Save
