9a7236af991503a0ea8ef9d55c5a2cde533ea8c3
taishikato <taishi.k0903@gmail.com>
2022-09-02 00:24:33 -0700

    update getNoteList

--- extension/src/_utils/getNoteList.ts
+++ extension/src/_utils/getNoteList.ts
@@ -1,22 +1,26 @@
-import type { Note } from "../types";
+import type { Note, NoteProperty } from "../types";
 
 const isProd = import.meta.env.PROD;
 
 export const getNoteList = isProd
-  ? async () => {
+  ? async (): Promise<NoteProperty[]> => {
       const notes: Note = await chrome.storage.sync.get(null);
       return Object.keys(notes).map((key) => ({
         key,
         content: notes[key].content,
+        created_at: notes[key].created_at,
+        modified_at: notes[key].modified_at,
       }));
     }
-  : () => {
+  : (): NoteProperty[] => {
       if (Object.keys(localStorage).length === 0) return [];
 
       return Object.keys(localStorage).map((noteKey) => {
         return {
           key: noteKey,
           content: JSON.parse(localStorage[noteKey] ?? "").content,
+          created_at: JSON.parse(localStorage[noteKey] ?? "").created_at,
+          modified_at: JSON.parse(localStorage[noteKey] ?? "").modified_at,
         };
       });
     };
