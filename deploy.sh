2a4c67ca3aa5a3c313b1c25167b8d331caa7c04d
taishikato <taishi.k0903@gmail.com>
2022-09-06 18:49:12 -0700

    update build command

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ rm -rf ./extension/node_modules
 
 mkdir copy
 
-find ./ -not -name copy -not -name . -not -name .git -not -name .gitignore -not -name .zipignore -not -name extension -not -name deploy.sh -not -name extenson.zip -maxdepth 1 -exec cp -R {} copy \;
+find ./ -not -name copy -not -name . -not -name .git -not -name .gitignore -not -name .zipignore -not -name extension -not -name deploy.sh -not -name extenson.zip -not -name deploy.sh -not -name clean.sh -not -name README.md -maxdepth 1 -exec cp -R {} copy \;
 
 zip -r -q extension.zip ./copy
 
